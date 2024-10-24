172189d6a4bbf934260dbc8c5b41df6e15c18833
Hooman <hoomantahani@gmail.com>
2024-10-24 09:05:47 +0330

    feat: input output examples

---
+++ 01-input-output.py
@@ -0,0 +1,11 @@
+# s=input("Enter your name:");
+s = "h"
+print(s, s, sep="-")
+print(s, end=" ")
+print("2")
+
+name = input("Enter Name:")
+age = input("Enter your age:")
+print("Hi", name, "you are", age, "years old")
+ageN = int(age)
+ageN += ageN
